d5ecdd647f563d74f8025197c2f151a4b2ca5e8d
Brooks Patton <brooks@brookzerker.com>
2017-02-28 22:45:42 -0700

    dyslexised the port line

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const store = {}
 
 app.use(bodyParser.urlencoded({ extended: false }))
